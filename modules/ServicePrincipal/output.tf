e9695b892d3e87608e3f4ebb1b1bb2a9c43505e9
Yash Negi <91847534+yAsH-nEgI@users.noreply.github.com>
2025-05-21 12:22:41 +0530

    updated modules

---
+++ modules/ServicePrincipal/output.tf
@@ -0,0 +1,19 @@
+output "service_principal_object_id" {
+  description = "The object id of service principal. Can be used to assign roles to user."
+  value       = azuread_service_principal.main.object_id
+}
+
+output "service_principal_tenant_id" {
+  value = azuread_service_principal.main.application_tenant_id
+}
+
+output "client_id" {
+  description = "The application id of AzureAD application created."
+  value       = azuread_application.main.client_id
+}
+
+output "client_secret" {
+  description = "Password for service principal."
+  value       = azuread_service_principal_password.main.value
+ 
+}
---
+++ modules/ServicePrincipal/variables.tf
@@ -0,0 +1,4 @@
+variable "service_principal_name" {
+  type        = string
+  description = "The name of the service principal to create"
+}
